1fd941b4604e8772ee63bb88d708b3c3127ec67e
Leonardo Elnisky <leonardoelnisky@hotmail.com>
2019-06-15 00:24:00 -0300

    Logout fixed

--- resources/views/logout.blade.php
+++ resources/views/logout.blade.php
@@ -1,15 +1,7 @@
 <?php
-    //session_start();
-    
-    //dd(Session::get('usuario'));
     Session::forget('usuario');
-    
-    //dd(Session::get('usuario'));
-    //Session::flush();
-    //unset($_SESSION["usuario"]);
-    //session_destroy();
-    //view('home');
-    //header('location:/');
-    route('index.index');
-    //exit;
+    Session::save();
+
+    header('location:/index');
+    exit;
 ?>
--- storage/framework/views/8a928114cef13c5ad570aa37389d5c13147df9df.php
+++ storage/framework/views/8a928114cef13c5ad570aa37389d5c13147df9df.php
@@ -3,13 +3,14 @@
     
     //dd(Session::get('usuario'));
     Session::forget('usuario');
+    Session::save();
     
     //dd(Session::get('usuario'));
     //Session::flush();
     //unset($_SESSION["usuario"]);
     //session_destroy();
     //view('home');
-    //header('location:/');
-    route('index.index');
-    //exit;
+    header('location:/index');
+    //route('index.index');
+    exit;
 ?><?php /**PATH C:\xampp\htdocs\laravel\Bitep_Laravel\resources\views/logout.blade.php ENDPATH**/ ?>
