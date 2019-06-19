dcb221e4bcd487cfd7b60a5a1b42d633824a34af
Thomas Nichay <thomasnichay@yahoo.com.br>
2019-06-19 17:51:47 -0300

    storage limpado

--- storage/framework/views/3a148f6259b88e0803476af73bc153ae3b1e5274.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-unset($_SESSION["usuario"]);
-header('location:/');
-exit;
-?><?php /**PATH C:\xampp\htdocs\laravel\bitep\resources\views/logout.blade.php ENDPATH**/ ?>
--- storage/framework/views/8a928114cef13c5ad570aa37389d5c13147df9df.php
+++
@@ -1,7 +0,0 @@
-<?php
-    Session::forget('usuario');
-    Session::save();
-
-    header('location:/index');
-    exit;
-?><?php /**PATH C:\xampp\htdocs\laravel\Bitep_Laravel\resources\views/logout.blade.php ENDPATH**/ ?>
